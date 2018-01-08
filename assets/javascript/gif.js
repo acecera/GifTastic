de04a78714139f2150b50d2fdfa2e75eda62f846
Anthony Cera <ace_cera@yahoo.com>
2018-01-07 22:03:15 -0800

    deleted a line and moved the start/stop function to inside of .done function. Still can't figure out 401 error.

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,20 +3,20 @@ $(document).ready(function() {
     "Leopard", "Snake", "Jaguar"]; 
     var apiKey = "9Yg1G0Q2WcHlIz7JXSNh1kUJ3bvl9jDR"
 })
-
+//on click function to get gif every time an animal button is pressed 
 $("button").on("click", function() {
     var animal = $(this).attr("data-animal");
     var apiKey = "9Yg1G0Q2WcHlIz7JXSNh1kUJ3bvl9jDR"
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
     animal + apiKey + "&limit=10";
-
+//ajax call for queryURL 
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .done(function(response) {
         console.log(queryURL);
-        //console.log(response);
+        console.log(response);
         //var results = response.data;
         //for (var i = 0; i < results.length; i++) {
         //if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
@@ -30,6 +30,20 @@ $("button").on("click", function() {
             //$("#animals").append(animalDiv);
         //}
       //}
+    })
+    //on click function to start and stop gif
+
+    $("animalImage").on("click", function() {
+        var still = $(this).attr("data-still");
+        var animate = $(this).attr("data-animate");
+
+        if (state === 'still') {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
     })
 
     
