f12f4d7eb0eb7b90fc1e6db32b9fc738162c3046
Anthony Cera <ace_cera@yahoo.com>
2018-01-07 19:01:22 -0800

    added an array of animals to the gif.js file. Added more buttons to html file. added document ready function

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,8 +1,14 @@
-$("#button").on("click", function () {
+$(document).ready(function() {
+    var animalGif= ["Tiger", "Dog", "Bear", "Lion", "Puma", "Alligator", "Cheetah", 
+    "Leopard", "Snake", "Jaguar"]; 
+    var apiKey = "9Yg1G0Q2WcHlIz7JXSNh1kUJ3bvl9jDR"
+})
+
+$("button").on("click", function() {
     var animal = $(this).attr("data-animal");
-    
+    var apiKey = "9Yg1G0Q2WcHlIz7JXSNh1kUJ3bvl9jDR"
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
-    animal + "&api_key=9Yg1G0Q2WcHlIz7JXSNh1kUJ3bvl9jDR&limit=10";
+    animal + apiKey + "&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -11,18 +17,20 @@ $("#button").on("click", function () {
     .done(function(response) {
         console.log(queryURL);
         console.log(response);
+        //var results = response.data;
+    //for (var i = 0; i < results.length; i++) {
+      //  if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+        //    var animalDiv = $("<div>");
+          //  var rating = results[i].rating;
+            //var p = $("<p>").text("Rating" + rating);
+           // var animalImage = $("<img>");
+           // animalImage.attr("src", results[i].object.data.url);
+           // animalDiv.append(p);
+           // animalDiv.append(animalImage);
+           // $("#animals").append(animalDiv);
+       // }
+   // }
     })
-    var results = response.data;
-    for (var i = 0; i < results.length; i++) {
-        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-            var animalDiv = $("<div>");
-            var rating = results[i].rating;
-            var p = $("<p>").text("Rating" + rating);
-            var animalImage = $("<img>");
-            animalImage.attr("src", results[i].images.fixed_height.url);
-            animalDiv.append(p);
-            animalDiv.append(animalImage);
-            $("#animals").append(animalDiv);
-        }
-    }
+
+    
 });
